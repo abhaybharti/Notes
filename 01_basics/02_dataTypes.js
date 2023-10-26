156b44619988613362df32d168f37d12db0076a4
Abhay Bharti <abhaybharti@users.noreply.github.com>
2023-10-26 13:54:57 +0000

    data types

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,3 +1,14 @@
 "use strict" //treat all JS code as newer version in whole file
 
 // alert(3+3) // cant use in nodejs, can be used browser. Currently, we are running in nodejs
+let name = "Abhay" //String data types
+let age =18 // number data types
+let isLoggedIn = false //boolean data types -- true/false
+let ab = null // it is a standalone value, its a type. repretensation of empty value
+let abc = undefined // when value is not defined or assigned
+// symbol - used to uniquley define a component
+
+// object 
+
+console.log(typeof undefined); //undefined reason value not assigned
+console.log(typeof null); //is a type
