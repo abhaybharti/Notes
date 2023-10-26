9c5412b4c0d653eca6083cc979ac41cef612ec29
Abhay Bharti <abhaybharti@users.noreply.github.com>
2023-10-26 14:17:17 +0000

    conversion

--- 01_basics/03_conversonOperations.js
+++ 01_basics/03_conversonOperations.js
@@ -2,9 +2,9 @@ let score = "abh";
 console.log(typeof score);
 console.log(typeof(score));
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 
 //"33" => 33 can be converted easily
 //"33abc" => NaN (Not a number), typeof returns number for this
@@ -12,8 +12,8 @@ console.log(valueInNumber);
 
 let isLoggedIn = "Abhay";
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
-console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
 
 //1  in Boolean is => true
 //0 in Boolean is => false
@@ -21,5 +21,18 @@ console.log(typeof booleanIsLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//********Operations ************
+let value =3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 ="1"
+let str2 = 2
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+"1:23 https://www.youtube.com/watch?v=sscX432bMZo&t=2379s"
