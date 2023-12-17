d5b6fc889ab105e8d8f65fbe370b2955c666599b
Abhay Bharti <abhaybharti@users.noreply.github.com>
2023-12-17 03:48:57 +0000

    conversion of int/string/boolean

---
+++ 01_basics/02_1_dataConversion.js
@@ -0,0 +1,24 @@
+let score = true; 
+
+let valueInNumber = Number (score);
+console.log(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/* Outcome : 
+1. if score = 33, output will be number & 33
+2. if score= "33" output will be number & 33
+3. if score = "33abc" output will be number & NaN
+4. if score = null output will be number & 0
+5. if score = undefined output will be number & NaN
+6. if score = true output will be number & 1
+*/
+
+/* Boolean conversion
+Boolean(score);
+1 => true; 0 => false
+"" => false
+"abhay" => true
+*/
+
+/* To Do : All possible Data Type Conversion*/
