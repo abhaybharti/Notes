4415e9ebb721eb8465705a3d359b243e5be382b4
Abhay Bharti <abhaybharti@users.noreply.github.com>
2023-12-23 06:42:23 +0000

    array methods

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -20,5 +20,27 @@ marvelHeroes.push(DCHeroes); // adds second array as child array, does not merge
 console.log(marvelHeroes);
 
 const newarray = marvelHeroes2.concat(DCHeroes2); //merges two arrays, return a new array, does not modify original array
-console.log(newarray);
+console.log("Merge Array Using concat Operator : ",newarray);
 
+//conact method is not much used, a better approach is using spread operator
+const newArrayUsingSpread = [...marvelHeroes2,...DCHeroes2];
+console.log("Merge Array Using Spread Operator : ",newArrayUsingSpread);
+
+//How to flat an array
+const anArray = [1,2,3,[4,5,6],7,[6,7,[4,5]]];
+
+const flatArray =anArray.flat(Infinity); // flatten the array till inifinite hierarchy or flatten the arrays into single array
+console.log("Flatten the array : ",flatArray);
+
+//How to check if array
+console.log("isArray : ",Array.isArray("AB"));
+console.log("isArray : ",Array.isArray(flatArray));
+
+//Converts String to array
+console.log("Converts String to array : ",Array.from("AB"));
+
+//Converts variables to array
+let s1 = 300;
+let s2 = 400;
+let s3 = 500;
+console.log("Converts varaible to array : ",Array.of(s1,s2,s3));
